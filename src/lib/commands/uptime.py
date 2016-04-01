d124332be4ab650076787d430861135bb2a15659
shane <haroboy876@gmail.com>
2016-03-31 21:51:53 -0700

    add uptime command

--- src/lib/commands/uptime.py
+++ src/lib/commands/uptime.py
@@ -1,10 +1,16 @@
-# from src.lib.twitch import *
-#
-#
-# def uptime(**kwargs):
-#     channel = kwargs.get("channel", "testchannel")
-#     uptime = get_stream_uptime(channel)
-#     if uptime is not None:
-#         return "The current !uptime is " + str(uptime)
-#     else:
-#         return channel + " is offline!"
+from src.lib.twitch import Twitch
+from datetime import datetime
+
+
+def uptime(**kwargs):
+    channel = kwargs.get("channel", "testchannel")
+    format = "%Y-%m-%d %H:%M:%S"
+    stream = Twitch(channel, None).stream()["stream"]
+    if stream is not None:
+        start_time = str(stream.get('created_at')).replace(
+            "T", " ").replace("Z", "")
+        stripped_start_time = datetime.strptime(start_time, format)
+        time_delta = (datetime.utcnow() - stripped_start_time)
+        return "The current !uptime is " + str(time_delta).split(".")[0]
+    else:
+        return channel + " is offline!"
