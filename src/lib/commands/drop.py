8fd76834288f7f4ca21d2e1fe1532b5f0ebd8214
shane <haroboy876@gmail.com>
2016-04-17 17:03:10 -0700

    fix drop command

--- src/lib/commands/drop.py
+++ src/lib/commands/drop.py
@@ -8,7 +8,7 @@ SUPERUSER = config["superuser"].lstrip("#")
 def drop(**kwargs):
     channel = kwargs.get("channel")
     username = kwargs.get("username")
-    if username != SUPERUSER or username != channel:
+    if username != SUPERUSER and username != channel:
         return
     amount_to_divy = random.randint(20, 100)
     globals.CHANNEL_INFO[channel.lstrip("#")]["drop"] = {
